c66e953dfc1f29f6cb12e5d1f9b7f85148e4324f
Anthony Cox <anthonyjohnc@gmail.com>
2022-04-09 22:02:10 +0100

    ensure all test modules are exported

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {
+  configureSnapshotTests,
+  configureUnitTests
+} from './config/config';
+
+import TestDev from './test-development/TestDev';
+
+export {
+  configureSnapshotTests,
+  configureUnitTests,
+  TestDev
+};
